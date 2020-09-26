5db666649e70ea01be675ad202f64931a8be04fc
Christopher Danko <christopher.p.danko-1@ou.edu>
2020-09-25 22:58:33 -0500

    k

--- R/Probit3.R
+++ R/Probit3.R
@@ -5,22 +5,24 @@ source("DataGen3.R")
 library(lmtest)
 library(ivpack)
 
-probitboots <- function(trial){
-  bootse <- c()
-  rows = sample(1:1000, 1000, replace = TRUE)
-  bootdat <- dat[rows, ]
-  booty <- as.matrix(bootdat[, 1], ncol=1, nrow=n)
-  bootx <- as.matrix(bootdat[, 2], ncol=1, nrow=n)
-  bootxo <- as.matrix(bootdat[, (3+instrument):(2+exo+instrument)], ncol=exo, nrow=n)
-  bootz <- as.matrix(bootdat[, 3:(2+instrument)], ncol=instrument, nrow=n)
-  bootcf1 <- lm(bootx~bootz+bootxo)
-  bootv = bootcf1$residuals
-  bootcf2 <- glm(booty ~ bootx + bootxo + bootv , family = binomial(link = "probit"), control=list(epsilon = 1e-8, maxit = 100))
-  bootse <- bootcf2$coefficients[2]
-  return(bootse)
-}
-
 probit <- function(datmat, exo=1, instrument=1){
+  probitboots <- function(bootsize=599){
+    bootse <- c()
+    for (p in 1:bootsize){
+      rows = sample(1:1000, 1000, replace = TRUE)
+      bootdat <- dat[rows, ]
+      booty <- as.matrix(bootdat[, 1], ncol=1, nrow=n)
+      bootx <- as.matrix(bootdat[, 2], ncol=1, nrow=n)
+      bootxo <- as.matrix(bootdat[, (3+instrument):(2+exo+instrument)], ncol=exo, nrow=n)
+      bootz <- as.matrix(bootdat[, 3:(2+instrument)], ncol=instrument, nrow=n)
+      bootcf1 <- lm(bootx~bootz+bootxo)
+      bootv = bootcf1$residuals
+      bootcf2 <- glm(booty ~ bootx + bootxo + bootv , family = binomial(link = "probit"), control=list(epsilon = 1e-8, maxit = 100))
+      bootse[p] <- bootcf2$coefficients[2]
+    }
+    return(bootse)
+  }
+  
   r6 <- matrix(0, nrow=nrow(datmat), ncol= ncol(datmat))
   results <- matrix(0, nrow=ncol(datmat), ncol= 1)
   
@@ -58,9 +60,7 @@ probit <- function(datmat, exo=1, instrument=1){
       probitcf2 <- glm(y_values ~. , family = binomial(link = "probit"), data = cfdata, control=list(epsilon = 1e-8, maxit = 100))
       r6[i, j] <- probitcf2$coefficients[2]
       
-      trials <- seq(1:500)
-      results <- as.data.frame(lapply(trials,'probitboots'))
-      cprobit <- sd(results[1,])
+      cprobit <- probitboots()
       c6[i,j] <- cover(estimate = r6[i,j], se=cprobit)      
     }  
     results[j, 1] <- mean(abs(r6[, j]-0.5))
